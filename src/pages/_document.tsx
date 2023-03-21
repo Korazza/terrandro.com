0a9a54a0799624bcd2ba0048ef04b546cc40b984
Korazza <corattiroberto@gmail.com>
2023-03-21 16:52:41 +0100

    chore: add meta theme-color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,16 @@ export default function Document() {
 				<meta charSet='UTF-8' />
 				<meta httpEquiv='Content-Type' content='text/html; charset=UTF-8' />
 				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta
+					name='theme-color'
+					content='#bef264'
+					media='(prefers-color-scheme: light)'
+				/>
+				<meta
+					name='theme-color'
+					content='#a3e635'
+					media='(prefers-color-scheme: dark)'
+				/>
 			</Head>
 			<body>
 				<Main />
