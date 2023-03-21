2facbd2f03ae719390f350b1a4334294bc8d4960
Korazza <corattiroberto@gmail.com>
2023-03-21 17:02:33 +0100

    chore: make theme-color interactive

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,3 +1,6 @@
+import Head from 'next/head'
+
+import { useDarkMode } from '@/hooks/useDarkMode'
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
 import Footer from '@/components/Footer'
@@ -7,8 +10,16 @@ interface Props {
 }
 
 export default function PageLayout({ children }: Props) {
+	const { darkMode } = useDarkMode()
 	return (
 		<>
+			<Head>
+				{darkMode ? (
+					<meta name='theme-color' content='#1e293b' />
+				) : (
+					<meta name='theme-color' content='#a3e635' />
+				)}
+			</Head>
 			<Navbar />
 			<Sidebar />
 			<main className='relative top-20 md:left-40 md:max-w-[calc(100%-10rem)] max-h-[calc(100%-5rem)] w-full'>
