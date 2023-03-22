dbc0b82ab2038519ebe0a78d3a9ddaedbb6ab9d7
Korazza <corattiroberto@gmail.com>
2023-03-22 15:18:18 +0100

    chore: refactor tooltip classes

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -7,7 +7,7 @@ export default function Tooltip({ children, text }: Props) {
 	return (
 		<div className='group relative inline-block'>
 			{children}
-			<span className='invisible group-hover:md:visible absolute top-full mt-2 left-1/2 -translate-x-1/2 z-[1000] px-3 py-2 text-sm font-medium whitespace-nowrap text-slate-300 bg-slate-600/75 dark:bg-slate-700/75 backdrop-blur-sm rounded-lg shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out'>
+			<span className='invisible md:group-hover:visible absolute top-full mt-2 left-1/2 -translate-x-1/2 z-[1000] px-3 py-2 text-sm font-medium whitespace-nowrap text-slate-300 bg-slate-600/75 dark:bg-slate-700/75 backdrop-blur-sm rounded-lg shadow-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out'>
 				{text}
 			</span>
 		</div>
