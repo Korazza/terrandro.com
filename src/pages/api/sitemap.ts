c7e001e3b83e9f6287956fe3840ad138b9859021
Korazza <corattiroberto@gmail.com>
2023-03-23 12:41:47 +0100

    chore: change sitemap loc

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -1,11 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const WEBSITE_URL = 'https://terrandro.com'
+
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
 	res.setHeader('Content-Type', 'text/xml')
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>http://terrandro.com/</loc>
+      <loc>${WEBSITE_URL}/</loc>
       <lastmod>${new Date().toJSON().substring(0, 10)}</lastmod>
     </url>
   </urlset>`
